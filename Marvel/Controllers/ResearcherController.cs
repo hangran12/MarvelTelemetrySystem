e8c5e8b67bd555b58367f5e525e3f4bfad97ef51
Phong Nguyen <nguyenphong@seattleu.edu>
2019-05-21 18:33:00 -0700

    Added Researcher controller

---
+++ Marvel/Controllers/ResearcherController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marvel.Controllers
+{
+    public class ResearcherController : Controller
+    {
+        // GET: Researcher
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
