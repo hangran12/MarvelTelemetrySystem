fee345ed03d5a36f812adca3d996ba80c4231604
Phong Nguyen <nguyenphong@seattleu.edu>
2019-05-31 14:16:36 -0700

    Removed all database and model

--- Marvel/Controllers/AdminController.cs
+++ Marvel/Controllers/AdminController.cs
@@ -31,98 +31,19 @@ namespace Marvel.Controllers
         }
 
 
-        public ActionResult ArmstrongClinicReport(int page = 1, string sort = "SuccessRate", string sortdir = "asc", string search = "")
+        public ActionResult ArmstrongClinicReport()
         {
-            int pageSize = 100;
-            int totalRecord = 0;
-            if (page < 1) page = 1;
-            int skip = (page * pageSize) - pageSize;
-            var data = GetArmstrong(search, sort, sortdir, skip, pageSize, out totalRecord);
-            ViewBag.TotalRows = totalRecord;
-            ViewBag.search = search;
-            return View(data);
+            return View();
         }
 
-        public ActionResult MarvelousClinicReport(int page = 1, string sort = "SuccessRate", string sortdir = "asc", string search = "")
+        public ActionResult MarvelousClinicReport()
         {
-            int pageSize = 100;
-            int totalRecord = 0;
-            if (page < 1) page = 1;
-            int skip = (page * pageSize) - pageSize;
-            var data = GetMarvelous(search, sort, sortdir, skip, pageSize, out totalRecord);
-            ViewBag.TotalRows = totalRecord;
-            ViewBag.search = search;
-            return View(data);
+            return View();
         }
 
-        public ActionResult ClinicFReport(int page = 1, string sort = "SuccessRate", string sortdir = "asc", string search = "")
+        public ActionResult ClinicFReport()
         {
-            int pageSize = 100;
-            int totalRecord = 0;
-            if (page < 1) page = 1;
-            int skip = (page * pageSize) - pageSize;
-            var data = GetClinicF(search, sort, sortdir, skip, pageSize, out totalRecord);
-            ViewBag.TotalRows = totalRecord;
-            ViewBag.search = search;
-            return View(data);
+            return View();
         }
-
-        public List<Armstrong> GetArmstrong(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
-        {
-            using (ArmstrongCinicDatabaseEntities2 dc = new ArmstrongCinicDatabaseEntities2())
-            {
-                var v = (from a in dc.Armstrongs
-                         where
-                                 a.SuccessRate.Contains(search)
-                         select a
-                         );
-                totalRecord = v.Count();
-                v = v.OrderBy(sort + " " + sortdir);
-                if (pageSize > 0)
-                {
-                    v = v.Skip(skip).Take(pageSize);
-                }
-                return v.ToList();
-            }
-        }
-
-        public List<Marvelou> GetMarvelous(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
-        {
-            using (MarvelousClinicDatabaseEntities dc = new MarvelousClinicDatabaseEntities())
-            {
-                var v = (from a in dc.Marvelous
-                         where
-                                 a.SuccessRate.Contains(search)
-                         select a
-                         );
-                totalRecord = v.Count();
-                v = v.OrderBy(sort + " " + sortdir);
-                if (pageSize > 0)
-                {
-                    v = v.Skip(skip).Take(pageSize);
-                }
-                return v.ToList();
-            }
-        }
-
-        public List<ClinicF> GetClinicF(string search, string sort, string sortdir, int skip, int pageSize, out int totalRecord)
-        {
-            using (ClinicFDatabaseEntities dc = new ClinicFDatabaseEntities())
-            {
-                var v = (from a in dc.ClinicFs
-                         where
-                                 a.SuccessRate.Contains(search)
-                         select a
-                         );
-                totalRecord = v.Count();
-                v = v.OrderBy(sort + " " + sortdir);
-                if (pageSize > 0)
-                {
-                    v = v.Skip(skip).Take(pageSize);
-                }
-                return v.ToList();
-            }
-        }
-
     }
 }
